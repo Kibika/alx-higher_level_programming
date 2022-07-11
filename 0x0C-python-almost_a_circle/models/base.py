658d84ca523d8534c2c8f5bbde886cf65b6d6aa9
Kibika <steshykibika@gmail.com>
2022-07-11 04:15:47 -0700

    base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Create a base class
+"""
+class Base():
+    """Base class"""
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """initialize class variables"""
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects       
