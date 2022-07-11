66baf940758b732d38a688b9e34275fdd9a5c36a
Kibika <steshykibika@gmail.com>
2022-07-11 07:32:07 -0700

    write to text file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Write string to text file
 """
+
+
 def write_file(filename="", text=""):
     """write to file module"""
     with open(filename, "w", encoding="utf-8") as f:
